3117c427d47a5172a4579a95c6666e37046b50cc
Alabi Oluwafunmilayo <kkoredesemi@gmail.com>
2025-05-06 09:44:08 +0100

    java

---
+++ script.js
@@ -0,0 +1,164 @@
+// console. log ("hello world");
+// variable ;
+// const (permanent);
+// var (not in use again);
+// let (can be reassign);
+// primitive data type;
+
+// strings  (can be used with) '' ""  ;
+// ("my name is Funmi")('Funmi is my name');
+
+// number 
+// letmy Num = 64;
+
+// boolen (true/ false);
+//let  me =true;
+// let you= false;
+
+// null
+
+// non primitive data types;
+
+// arrary: can be string,object and number;
+//const first array = {10, 20, 30, 40};
+// const second array = [10, 'a string',{prop: "shola"}, [1,2] ;
+
+// object: 
+// const {
+ // name: "Sarah" , 
+ // my age:39 ,
+ // sex: male,
+// };
+
+// Arimethic operator
+// let b = 6;
+// let c =3;
+
+// addition 
+// const (a+b);
+// console.log (20);
+
+// subtraction
+// const (b-c);
+// console.log (3);
+
+// mulitiplication
+// const (8*c);
+// console.log (18);
+
+// division
+// const (b/c)
+// console.log (2);
+
+// modulus (gives us the remainder after dividing);
+// let d = 4;
+// let e =3;
+// const (d % 3);
+// console.log (1);
+
+// increment (++)
+//  const (d ++);
+// console.log (5);
+
+// decrement (--)
+// const (e --);
+// console. log  (4);
+
+// Assigment operator
+// letx = 10;
+// x= x+5 =15
+
+// +=, -=, *=, /=
+
+// comparison operators
+// ==, ====, !=, !==, 
+
+// const a = "Bayo";
+// const  b = "James"; 
+// const c = "John";
+// const  d = "James"; 
+// const f = 45;
+// const g = 50;
+// const h = "45";
+//const f= 43;
+
+// console.log  (g > h)
+//  Greater than
+
+// console.log ( h < 50 )
+// Less than
+
+// console.log ( b== d )
+// equal
+
+// console.log ( h ==== f)
+// strictly equal to
+
+// console.log ( f <= h)
+// less than equal to;
+
+// console.log (h >= f)
+// greater than equal to ;
+
+// !==, !== (not)
+
+// logical operators 
+// && And ( one true turns all to false);
+// || or (one true turns to become true at the end of te day);
+// ! not (opposite of any answer);
+
+// exercie 
+// const result !((40/20) === 2 && true) || ("yes" === "no")
+//const step1 = 40/20
+// console.log  = 2;
+//const step2 = step1 === 2;
+// console. log step2 (true);
+// const step3 = step2 && true
+// console. log = (true);
+// const step5 = "yes" === "no"
+// console.log =(false)
+//  const step6 =step3 || step 5
+// console.step =(true)
+// const step 7 = !(step6)
+//console. log =(false)
+
+// Condition 
+// if ( "some string" === "another string ") {
+  //  console.log ("the string ");}
+    
+  else {
+    //console.log ('the string are not equal');
+
+  }
+  // the strings are not equal
+
+// if condition (returns true)[]
+
+// const firstnumber= 20;
+// const secondNumber = 10;
+// const jsExpression = firstNumber> secondNumber; // true
+
+// if else statement () {} else []
+
+
+// prompt
+// prompt("enter your name")
+// const myAge = prompt("enter gour age")
+// console.log (myAge)
+
+
+let number= parseInt(prompt("enter your number"))
+if (number%2===0){
+    alert(`${number} is even`)
+}
+else{
+    alert(`${number} is odd`)
+}
+
+
+
+
+
+
+
+
