d81b795a78e3e856430037f29c83b34b15ca88ca
Alabi Funmi <kkoredesemi@gmail.com>
2025-05-15 13:43:19 +0100

    Add files via upload

---
+++ methods and properties/script.js
@@ -0,0 +1,92 @@
+// )
+ let nameBox = ["ife", "seun", "david", "ayo", "charles", "peter"]
+//  nameBox[0]
+//  // to remove
+//  console.log (nameBox.pop())
+
+//   console.log (nameBox[0]
+
+//   )
+//   console.log (nameBox.length
+
+//   )
+//   console.log (nameBox.length-1
+
+//   )
+sconsole.log (nameBox.push("john")
+
+ )
+//   console.log(nameBox[5])
+// to add more names to the array just do
+// nameBox.push(John)
+// nameBox.push("tosin")
+// console.log(nameBox);
+// // To remove name from the arrary  
+// // .pop()
+// // to remove the first 
+// //.nameBox.shift()
+// // to add the name back 
+// //.nameBox.unshift()
+// // to check if an item is in the array 
+// console.log(nameBox.includes ("ayo"));//true
+// // to check the index position in th arrary 
+// console.log(nameBox.indexOf("seun"));// 1
+// // names not in the box will record -1
+// console.log(nameBox.indexOf("funmi"));// -1
+// // conversion from array to strings
+// console.log (nameBox.join ());// it will be on a straight line
+// // to remove the comma on a straight line
+// console.log()
+// // slice to pick from the array. you pick with their index.
+// console.log(nameBox.slice(0,1));
+// console.log(nameBox.slice(2,4));
+// // splice to delete, update and add to an array 
+// console.log (nameBox.splice(4,0,"Tope"));
+// console.log(nameBox)
+// console.log (nameBox.splice(2,4));
+// console.log(nameBox)
+// // the same array are not === the same even with the same content. 
+// // this is because the links are different.
+
+// objects
+//  let studentData
+//  {name:"ope"}
+//  console.log(studentData.score:100
+// )
+let studentData
+ {name:"ope"} 
+let scoreboard =[ 1,2, 3,4,5]
+console.log(scoreboard.push("ife"));
+console.log(scoreboard
+
+)
+ 
+let classinfo = {name:"shola", age:20}
+console.log(classinfo.name)
+console.log(classinfo.age)
+console.log(scoreboard.push(classinfo));
+console.log(scoreboard
+
+)
+console.log(scoreboard[6
+
+])
+console.log(classinfo.name
+
+)
+console.log(classinfo.age)
+// question on array,  how would you access the third element?,How do you find the number of elements in an array?
+//How do you find the number of elements in an array?
+
+
+let arr = [10, 20, 30, 40, 50];
+console.log(arr[3])
+console.log (arr)
+
+
+
+
+
+
+
+
