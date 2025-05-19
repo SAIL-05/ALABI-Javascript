74331e5994b3b0286bbcff5446be7928b4237fd5
Alabi Funmi <kkoredesemi@gmail.com>
2025-05-19 16:44:28 +0100

    Add files via upload

---
+++ javaclass/script.js
@@ -0,0 +1,45 @@
+// write a function that divide two numbers
+
+// function dividetwo (num1, num2){
+//   let result = num1/num2  
+//   console.log(result)
+
+// }
+// dividetwo(20, 2)
+
+// write a function that checks if a number is even or odd
+//  function tocheckoddoreven(){
+//     const num = parseInt(prompt("enter a number"))
+//     if (num %2=== 0){
+//         alert("is even")
+//     }
+//     else{
+//      alert("is odd")
+//     }
+//  }
+//  tocheckoddoreven()
+
+//  // write a function to return the largest number
+//  function largestnumber(num1, num2, num3){
+
+//     num1= (prompt("enter a number"))
+//      num2 =(prompt("enter a number"))
+//      num3= (prompt("enter a number"))
+//      console.log (Math.max (num1, num2, num3))
+//  }
+//  largestnumber()
+ 
+ // write a function that reverse a string
+ function toreverse (input){
+    input = "Alabi"
+    return input.split('').reverse().join('');
+ }
+ console.log(toreverse("Alabi"))
+
+ // find the longest of word in a sentence
+ function longestword () {
+    input = "shola is a girl"
+    return input.split ("").lenght
+ }
+ console.log(longestword ("shola is a girl"))
+ 
