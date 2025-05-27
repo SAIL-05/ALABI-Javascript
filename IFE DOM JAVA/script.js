6c5722865a4fa2ddd1c7e689d3c5da0112642e4b
Alabi Funmi <kkoredesemi@gmail.com>
2025-05-27 20:43:43 +0100

    Add files via upload

---
+++ IFE DOM JAVA/script.js
@@ -0,0 +1,56 @@
+ console.log('welcome to sail')
+
+ console.log(document.getElementsByClassName ("main"))
+
+
+ console.log(document.getElementsByClassName('myself'))
+
+
+ console.log(document.querySelectorAll("h1"))
+
+
+ console.log(document.querySelector("h1"))
+
+let student = {
+     name: "Tayo",
+     nationality : "American", 
+     status: "Richest in Africa",
+
+     innnerText: "", 
+     innerHTML :"",
+
+ }
+ student.name
+console.log(document.querySelectorAll ("li")[2].innerText)
+document.querySelectorAll('li')[4].innerText = "dodo"
+document.querySelectorAll('li')[0].innerText = 'orange'
+ let myself =document.getElementById('myself')
+ myself.style.color = "red"
+ myself.style.backgroundColor = "gold"
+
+
+ 
+
+
+const showAlert = ()=>{
+    alert("welcome")
+}
+document.getElementById("lick")
+function addText (){
+
+    if (lick.innerText == "closed"){
+        lick.innerText = "open"
+    }
+    else{
+        lick.innerText = "closed"
+    }
+    
+}
+
+
+let header = document.getElementsByClassName('heading')
+
+function opener(){
+    document.getElementsByClassName("heading")[0].textContent = "closed"
+}
+
