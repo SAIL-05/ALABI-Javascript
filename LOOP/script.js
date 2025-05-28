a40bf72d05dbd594b472fa86524ee661564ef3df
Alabi Funmi <kkoredesemi@gmail.com>
2025-05-28 04:45:32 +0100

    Add files via upload

---
+++ LOOP/script.js
@@ -0,0 +1,68 @@
+//function greetMe(name){
+    
+//}
+// for (initializer, conditionals, increment){
+
+// }
+// if(i<10)
+  for(let i= 0; i<10; i ++){
+ console.log("This is the value of i", i)
+ }
+// console.log("we are learning loops")
+
+// for (let startingIndex = 1; startingIndex<50; startingIndex ++){
+//     if(startingIndex%2 === 0){
+//         console.log(startingIndex, 'is an even number')
+//     }else{
+//         console.log(startingIndex, 'is an odd number')
+//     }
+// } 
+// for(let i= 5; i<=20; i++ ){
+//     console.log("This is the value of i", i)
+ let num =1 
+ let score = [20,23, 67,56, 78,70, 50, 90,56,34,]
+ for (let i=0; i<10; i++){
+ console.log ('the value of score is', score[i]) 
+ }
+// console.log(score[1]) 
+// console.log(score [num])
+
+// //Print numbers from 1 to 100 using a for loop
+// for(let i = 0; i <100; i++ ){
+//     console.log('this is the value of i', i)
+// }
+// //Print even numbers from 1 to 50 using a while loop.
+// for(let i= 0; i<50; i++){
+//     if(i %2===0){
+//         console.log(i,'is a even number')
+//     }
+// }
+//   //Calculate the sum of numbers from 1 to 10.
+//   let sum = 0;
+// for (let i = 1; i <= 10; i++) {
+//   sum += i;
+// }
+// console.log("Sum from 1 to 10 is:", sum);
+
+// // Print the multiplication table of a number (e.g. 7) using a loop.
+// let multiplication= 7
+// for(let i =7; i<10; i++){
+//   multiplication *= i++;
+// }
+// console.log('multiplication table is',multiplication );
+
+// //Find the maximum number in an array using a loop.
+// let array = [24, 56,78, 89,100.]
+// for(i =0; i<5; i++){
+//   console.log('the number is ',array[i] )
+// }
+// //Print the multiplication table of a number (e.g. 7) using a loop.
+// let number = 7;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${number} x ${i} = ${number * i}`);
+// }
+
+for(let i=1; i<= 50; i++){
+  console.log(i)
+}
