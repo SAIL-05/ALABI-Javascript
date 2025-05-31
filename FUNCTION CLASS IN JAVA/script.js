2ab97fea19006e6c4b582a9bff093ff1b5d28d9b
Alabi Funmi <kkoredesemi@gmail.com>
2025-05-31 23:59:29 +0100

    Add files via upload

---
+++ FUNCTION CLASS IN JAVA/script.js
@@ -0,0 +1,143 @@
+// function greet(name){
+//     let name2 = name
+//     console,log("Hello World",name, gender);
+// }
+// greet ("ife", "male", 90)
+// greet ("sam")
+// // for mutliple parameter 
+// function returnsumof2numbers (num1, num2){
+//  let result = num1+ num2   
+// }
+// console.log(result)
+// let sumof3numbers = function(num1, num2, num3){
+//     let result = num1+num2+num3
+//     console.log
+// }
+//prompt
+//  function takeInputfromuser (){
+//  let userResponse = prompt("enter any number")
+//  }
+// takeInputfromuser()
+// function subtract2values(num1,num2){
+//     let result = num1- num2;
+
+// }
+//create a function that takes input from a user to check 
+//if the user number is greater than six
+// function takeInputfromuser (){
+//    let input = prompt("enter a number")
+//     if (input > 6){
+//         console.log(`greater than 6`)
+//     }else{
+//         console.log("lesser than 6")
+//     }
+
+// }
+// takeInputfromuser()
+// // create a function to that takes input from aa user and it should count the characters in it
+// function takenamefromuser (){
+//     let name= prompt("enter a name")
+//     console.log("your name is",name.length ,"long")
+
+// }
+// //takenamefromuser("Funmi")
+// takenamefromuser("Elujoba")
+
+// function numberhighest() {
+//     let num1 = parseInt(prompt("enter a name"))
+//      let num2 =parseInt(prompt("enter a name"))
+//       let num3 =parseInt(prompt("enter a name"))
+//        let num4 =parseInt(prompt("enter a name"))
+//     let result = Math.max (num1, num2, num3, num4)
+//     console.log(result)    
+// }
+//  numberhighest()
+
+//const greetMe = ()=> {
+
+// }
+// arrow function
+// let checkEvennumber= ()=> {
+//     let userNumnber = prompt ("Enter any number")
+//     if ((userNumnber %2) === 0) {
+//      console.log("number is even") 
+    
+//      }else {
+//         console.log("odd number")
+        
+//     }
+// }
+//checkEvennumber()
+
+// function greet(name){
+
+//     userresponse = "good morning"
+//     console.log ("Good morning","TOSIN ")
+
+
+// }
+// greet()
+//Write a program to sum all elements of an array.
+
+// Input: [1, 2, 3, 4] 
+
+// function addNum (a,b, c,d){
+//     console.log(a+b+c+d)
+// }
+// addNum(1, 2,3,4)
+// addNum(2,3,4,5)
+// addNum(5,6,9,10)
+
+
+//Write a program to find the largest of three numbers.
+
+// Input: 10, 25, 5 → 
+// function largestNum(){
+//     let num1 = prompt("enter a number")
+//     let num2 = prompt("enter a number")
+//     let num3 = prompt ("enter a number")
+//     let result = Math.max (num1, num2, num3)
+//     console.log(result)
+// }
+// largestNum()
+//Write a program to reverse a string
+// Input: "hello"
+//function reversestring(){ 
+
+//}
+//reversestring("hello")
+
+// function reverseString(str) {
+//     return str.split('').reverse().join('');
+  
+// }
+// console.log(reverseString ("noon"))
+
+// //Write a program to check if a number is prime number.
+// function isPrime(n) {
+//     if (n <= 1) return false;
+//     if (n === 2) return true;
+//     if (n % 2 === 0) return false;
+    
+//     for (let i = 3; i <= Math.sqrt(n); i += 2) {
+//         if (n % i === 0) return false;
+//     }
+
+//     return true;
+// }
+// const num = parseInt(prompt("Enter a number:"));
+
+if (isPrime(num)) {
+    console.log(`${num} is a prime number.`);
+} else {
+    console.log(`${num} is not a prime number.`);
+}
+
+// Write a function to find the square root of a number without using Math.sqrt.
+ //Write a function to capitalize the first letter of each word in a string.
+ function tocapitalletter(){
+    let name = ('')
+    console.log(name.charAt)
+
+ }
+ tocapitalletter("timothy paul")
